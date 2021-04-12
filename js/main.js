7298948b4a89073bd3421f82de86832e17ec10a4
Krasiukov Evhen <seocrafters@gmail.com>
2021-04-12 15:35:03 +0300

    mobile adaptation done

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@ $(function(){
     $('.slider__inner').slick({
         arrows: false,
         dots: true,
-        slidesToShow: 3
+        slidesToShow: 3,
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+                breakpoint: 577,
+                settings: {
+                  slidesToShow: 1
+                }
+              }
+          ]
         });
 });
